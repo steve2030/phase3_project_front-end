7f628454627ba97f1237e27c7ef9af0b26dc1b15
steve <stevemighty2030@gmail.com>
2022-11-09 19:01:45 +0300

    added a json data

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export default [
+    {
+        id: 1,
+        title: "The Hobbit",
+        genre: "Fiction",
+        author: "JRR Tolkien",
+        rating: 4.9,
+        image_url: "https://img.bookfrom.net/img/1707050857/1412_the_hobbit.jpg",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 2,
+        title: "Lord of the Rings",
+        genre: "Fiction",
+        author: "JRR Tolkien",
+        rating: 4.7,
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 3,
+        title: "The Calculating Stars",
+        genre: "Fiction",
+        author: "Mary Kowal",
+        rating: 4.5,
+        image_url: "https://media.npr.org/assets/img/2021/08/13/711gd93ifkl_custom-4da4cd0caaee24686552a01b6a9a5fe365b0e08d-s800-c85.jpeg",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 4,
+        title: "The Sound of Stars",
+        genre: "Romance",
+        author: "Alechia Dow",
+        rating: 4.9,
+        image_url: "https://img.buzzfeed.com/buzzfeed-static/static/2020-07/6/21/asset/84a4a5f842a9/sub-buzz-1226-1594072112-25.jpg?crop=315%3A473%3B0%2C1&resize=475%3A%2A",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 5,
+        title: "Day Zero",
+        genre: "Fiction",
+        author: "Robert Cargill",
+        rating: 4.9,
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1605822014l/49949416._SY475_.jpg",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 6,
+        title: "The Windup Girl",
+        genre: "Fantasy",
+        author: "Paolo Pacigalupi",
+        rating: 4.9,
+        image_url: "https://cdn.vox-cdn.com/thumbor/M8TJPhrrulC5ZaQFbVca87rzHtU=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/21960151/71UH89XIA8L.jpg",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 7,
+        title: "Romeo and Juliet",
+        genre: "Romance",
+        author: "William Shakespear",
+        rating: 4.8,
+        image_url: "https://m.media-amazon.com/images/I/711TxX8cdlL.jpg",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 8,
+        title: "The War of the Worlds",
+        genre: "Fiction",
+        author: "HG Wells",
+        rating: 4.9,
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGi-ROpVAt6UJQ7fC4NWqSlbH0Fn5hHLVQYI6mXbbAEFWDgsct8xKwWlB9V1Tenyt1v-M&usqp=CAU",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    },
+    {
+        id: 9,
+        title: "The Love Hypothesis",
+        genre: "Romance",
+        author: "Ali Hazelwood",
+        rating: 4.2,
+        image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1639425086-51uq6tDW6SL._SL500_.jpg?crop=1xw:1xh;center,top&resize=320%3A%2A",
+        description: "A book is a medium for recording information in the form of writing or images, typically composed of many pages (made of papyrus, parchment, vellum, or paper) bound together and protected by a cover."
+    }
+
+]
