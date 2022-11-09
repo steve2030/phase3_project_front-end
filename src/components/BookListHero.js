c86f0a12ba505e4361ca08ea8851d630966b565f
steve <stevemighty2030@gmail.com>
2022-11-09 19:22:17 +0300

    initialized the BookListHero component

---
+++ src/components/BookListHero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function BookListHero() {
+    return (
+        <div className="main-container">
+            <p className="desc">A review gives the reader a concise summary of the content. This includes a relevant description of the topic as well as its overall perspective, argument, or purpose.</p>
+            <div className="right-container">
+                <h3 className="quote">Books are uniquely portable magic</h3>
+                <p className="desc">-Vanessa Hugo</p>
+            </div>
+        </div>
+    );
+}
